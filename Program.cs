1d5f636bf711d6e30a8c5b55dca0df0c90d71865
arwiester <66913932+arwiester@users.noreply.github.com>
2020-10-27 21:03:40 -0400

    Add files via upload
    Week 1 Homework assignment. 
    Added welcome message and asked for user's name to save as a string variable.
    Asked the user to select a country to save/travel to and then used that selection in a 'save your money' message.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string selectedCountry;
+
+            // Prints welcome message to user
+            Console.WriteLine($"Welcome to the Vacation Budget Planner!");
+            
+            //Asks user for their name and saves it as a string variable
+            Console.WriteLine($"Please enter your name:");
+            userName = Console.ReadLine();
+
+            //Welcome message and travel option 
+            Console.WriteLine($"Welcome {userName}! Please enter the country you wanto to? Jamacia or Mexico");
+            selectedCountry = Console.ReadLine();
+            
+            //Saves the selected country as a string variable
+            Console.WriteLine($"You have selected {selectedCountry}. Save those dollars!");
+            Console.ReadLine();
+           
+        }
+    }
+}
